758601d8e7649b19aa5b042eadf018ebcb61ef78
depers <dev_fengxiao@163.com>
2024-03-12 22:43:44 +0800

    2024-03-12

--- src/cn/bravedawn/jd_121/Solution2.java
+++ src/cn/bravedawn/jd_121/Solution2.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author : depers
- * @program : leetcode
- * @date : Created in 2024/3/11 20:21
+ * @Author : fengx9
+ * @Project : leetcode-main
+ * @Date : Created in 2024-03-12 09:15
  */
 public class Solution2 {
 
@@ -15,8 +15,11 @@ public class Solution2 {
      */
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,2,6,1};
+        int[] nums = {1, 2, 3, 4, 5, 2, 6, 1};
         compute(1000, nums);
+
+        // 4000
+        // 8000
     }
 
 
@@ -35,32 +38,36 @@ public class Solution2 {
         int n = prices.length;
 
         for (int i = 0; i < n; i++) {
-
             if (i == n - 1) {
                 if (num == 0) {
                     list.add("hold");
                 } else {
                     list.add("sell");
                     money += num * (prices[i] - lastMoney);
+                    num = 0;
                 }
-            } else {
-                if(num == 0) {
-                    list.add("buy");
-                    num = money / prices[i];
-                    lastMoney = prices[i];
-                    continue;
-                }
+                continue;
+            }
 
-                if (prices[i] <= prices[i+1] && i < n - 1) {
+            if (i < n - 1 && prices[i] >= prices[i + 1] && prices[i] > lastMoney) {
+                if (num == 0) {
                     list.add("hold");
                 } else {
                     list.add("sell");
                     money += num * (prices[i] - lastMoney);
                     num = 0;
                 }
+
+            } else {
+                if (num == 0) {
+                    list.add("buy");
+                    lastMoney = prices[i];
+                    num = money / prices[i];
+                    money = 0;
+                } else {
+                    list.add("hold");
+                }
             }
-
-
 
         }
 
