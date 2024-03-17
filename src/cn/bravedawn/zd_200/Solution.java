730e5abeb0deeb76ce78d0c4208d319fb3c00d97
depers <dev_fengxiao@163.com>
2024-03-17 22:53:53 +0800

    2024-03-17

---
+++ src/cn/bravedawn/zd_200/Solution.java
@@ -0,0 +1,64 @@
+package cn.bravedawn.zd_200;
+
+/**
+ * @Author : fengx9
+ * @Project : leetcode-main
+ * @Date : Created in 2024-03-15 13:56
+ */
+public class Solution {
+
+    public static int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+
+    }
+
+
+    private static void dfs(char[][] grid, int r, int c) {
+        // 判断 base case
+        if (!inArea(grid, r, c)) {
+            return;
+        }
+        // 如果这个格子不是岛屿，直接返回
+        if (grid[r][c] != '1') {
+            return;
+        }
+
+        grid[r][c] = '2'; // 将格子标记为「已遍历过」
+
+        // 访问上、下、左、右四个相邻结点
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    // 判断坐标 (r, c) 是否在网格中
+    private static boolean inArea(char[][] grid, int r, int c) {
+        return '0' <= r && r < grid.length
+                && '0' <= c && c < grid[0].length;
+    }
+
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+
+        System.out.println(numIslands(grid));
+
+    }
+
+
+}
