9044365a9d60fc0054843dd2d1c67604d614ce7e
depers <dev_fengxiao@163.com>
2024-05-29 08:16:00 +0800

    2024-05-27

--- src/cn/bravedawn/zd/zd_236/Solution.java
+++ src/cn/bravedawn/zd/zd_236/Solution.java
@@ -4,6 +4,8 @@ package cn.bravedawn.zd.zd_236;
  * @author : depers
  * @program : leetcode
  * @date : Created in 2024/5/28 07:48
+ * 236. 二叉树的最近公共祖先
+ * 参考思路：https://programmercarl.com/0236.%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E6%9C%80%E8%BF%91%E5%85%AC%E5%85%B1%E7%A5%96%E5%85%88.html
  */
 public class Solution {
 
